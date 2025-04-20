8155866b9ccc19a103e854a9a08adeb2ed0ae3bd
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2025-04-20 20:16:36 +0500

    Upgrade features

--- init.sh
+++ init.sh
@@ -2,24 +2,44 @@
 
 source .env
 
-docker-compose build
+sed -i "s/_PHP_IMAGE_VERSION_/${PHP_IMAGE_VERSION}/" docker/php/Dockerfile
+sed -i "s/_NGINX_IMAGE_VERSION_/${NGINX_IMAGE_VERSION}/" ./docker-compose.yml
+sed -i "s/_POSTGRES_IMAGE_VERSION_/${POSTGRES_IMAGE_VERSION}/" ./docker-compose.yml
+sed -i "s/_MYSQL_IMAGE_VERSION_/${MYSQL_IMAGE_VERSION}/" ./docker-compose.yml
+sed -i "s/_REDIS_IMAGE_VERSION_/${REDIS_IMAGE_VERSION}/" ./docker-compose.yml
+sed -i "s/_NGINX_PORT_/${NGINX_PORT}/" ./docker-compose.yml
+sed -i "s/_MYSQL_PORT_/${MYSQL_PORT}/" ./docker-compose.yml
+sed -i "s/_MYSQL_TEST_PORT_/${MYSQL_TEST_PORT}/" ./docker-compose.yml
+sed -i "s/_POSTGRES_PORT_/${POSTGRES_PORT}/" ./docker-compose.yml
+sed -i "s/_POSTGRES_TEST_PORT_/${POSTGRES_TEST_PORT}/" ./docker-compose.yml
+sed -i "s/_REDIS_PORT_/${REDIS_PORT}/" ./docker-compose.yml
 
-docker-compose run --no-deps php composer create-project --prefer-dist laravel/laravel
-rm laravel/docker-compose.yml
-mv laravel/{,.[^.]}* .
+docker compose build
+
+docker compose run --no-deps --rm php composer create-project --no-scripts --prefer-dist laravel/laravel
+
+mv laravel/* .
+mv laravel/.* .
 rmdir laravel
+
 echo '' > README.md
-sed -i "/DB_HOST=/c\DB_HOST=${DB_HOST}" .env
-sed -i "/DB_DATABASE=/c\DB_DATABASE=${DB_DATABASE}" .env
-sed -i "/DB_USERNAME=/c\DB_USERNAME=${DB_USERNAME}" .env
-sed -i "/DB_PASSWORD=/c\DB_PASSWORD=${DB_PASSWORD}" .env
-echo '/.idea' >> .gitignore
 
-if [ "$(basename $PWD)" != "laradocker" ] ;
-then
+sed -i "/APP_URL=/c\APP_URL=http://localhost:${NGINX_PORT}" .env.example
+
+sed -i "/DB_CONNECTION=/c\DB_CONNECTION=pgsql" .env.example
+sed -i "/DB_HOST=/c\DB_HOST=${DB_HOST}" .env.example
+sed -i "/DB_DATABASE=/c\DB_DATABASE=${DB_DATABASE}" .env.example
+sed -i "/DB_USERNAME=/c\DB_USERNAME=${DB_USERNAME}" .env.example
+sed -i "/DB_PASSWORD=/c\DB_PASSWORD=${DB_PASSWORD}" .env.example
+
+echo >> .env.example
+echo "COMPOSE_BAKE=true" >> .env.example
+
+if [ "$(basename $PWD)" != "laradocker" ]; then
   rm init.sh
-
+  rm .env
   rm -fr .git
+
   git init
   git add .
   git commit -m 'Initial commit'
